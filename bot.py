07dde37e4fcfa61c8efd332225a1dac148122c68
Blue <Bluegamerdogyt@gmail.com>
2023-01-20 17:29:42 +0100

    Infoboard command v1

--- bot.py
+++ bot.py
@@ -281,10 +281,59 @@ async def mypoints(interaction: discord.Interaction):
         embed = discord.Embed(color=UserCommandsCOL, description=f"You have no points.")
     await interaction.response.send_message(embed=embed)
 
+class InfoboardOptions(discord.ui.Select):
+    def __init__(self):
+        options=[
+            discord.SelectOption(label="DSB Helper Infoboard", description="A list of all basic commands.", value="DHI"),
+            discord.SelectOption(label="Basic Commands", description="A list of all basic commands.", value="BC"),
+            discord.SelectOption(label="DSB Commands", description="A list of all commands available to DSB members.", value="DC"),
+            discord.SelectOption(label="Management Commands", description="A list of all commands available to DSBPC+.", value="MC"),
+            #discord.SelectOption(label="Squadrons Infoboard", description="Shows some general information about the server and squadrons.", value="SI")
+        ]
+        super().__init__(placeholder="Select a dropdown...", options=options, min_values=1, max_values=1)
+        
+    async def callback(self, interaction: discord.Interaction):
+        #await interaction.response.send_message(f'You chose {self.values[0]}.')
+        if self.values[0] == "DHI":
+            embed = discord.Embed(title="**DSB Helper Infoboard**", description="The DSB Helper mainly manages the points based quota system. Provided are infoboards with various commands and information related to the bot. See the dropdown menu below.", color=UserCommandsCOL)
+            embed.add_field(name="Credits", value="Bluegamerdog - Backend API and bot host\nObviously_Shush - Frontend & Misc\nThe suffering - Bug testing")
+            embed.set_footer(text="DSB Helper 1.0")
+            embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/lnHKwZ40MRhYKuNJpNUS8NQiaekqkgfW9TaGj-B5Yg0/https/tr.rbxcdn.com/580b2e0cd698decfa585464b50a4278c/150/150/Image/Png")
+        elif self.values[0] == "BC":
+            embed = discord.Embed(title="**Basic Commands**", description="All servers members can use these commands. They are represented by the color white.", color=BasiccommandCOL)
+            embed.add_field(name="**/whois**", value="Displays someones general and server informations.", inline=False)
+            embed.add_field(name="**/ping**", value="Shows the bot's response time in miliseconds.", inline=False)
+        elif self.values[0] == "DC":
+            embed = discord.Embed(title="**DSB Commands**", description="All DSB members PFC and above have access to these and are represented by the navy blue.", color=UserCommandsCOL)
+            embed.add_field(name="**/points overview**", value="Displays a leaderboard for points.", inline=False)
+            embed.add_field(name="**/points view**", value="View someone else's current point count.", inline=False)
+            embed.add_field(name="**/mypoints**", value="View your current point count.", inline=False)
+            embed.add_field(name="**/soup**", value="Adds or removes the Op. Supervisor Role. [EDS+]", inline=False)
+            embed.add_field(name="**/rloa**", value="Coming soon:tm:...", inline=False)
+        elif self.values[0] == "MC":
+            embed = discord.Embed(title="**Management Commands**", description="DSB Pre-Command and above have access to these. They are represented by the color black.", color=HRCommandsCOL)
+            embed.add_field(name="**/points add/remove**", value="Adds/removes points to/from a user.", inline=False)
+            embed.add_field(name="**/pointsreset**", value="Resets the points of all users to zero.", inline=False)
+            embed.add_field(name="**/updatequota**", value="Updates the quota block number, start and end date.", inline=False)
+            embed.add_field(name="**/restart**", value="Restarts the DSB Helper.", inline=False)
+            embed.add_field(name="**/shutdown**", value="Shuts down DSB Helper. [DSBCOMM+]", inline=False)
+        #elif self.values[0] == "SI":
+        #    embed = discord.Embed(title="**Server Info**")
+        
+        await interaction.response.edit_message(embed=embed)
+        
+class InfoboardView(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.add_item(InfoboardOptions())
+
 @bot.tree.command(name="infoboard",description="Shows bot information and a list of commands.")
 async def infoboard(interaction: discord.Interaction):
-    embed = discord.Embed(description="Coming sometime soon...")
-    await interaction.response.send_message(embed=embed)
+    embed = discord.Embed(title="**DSB Helper Infoboard**", description="The DSB Helper mainly manages the points based quota system. Provided are infoboards with various commands and information related to the bot. See the dropdown menu below.", color=UserCommandsCOL)
+    embed.add_field(name="Credits", value="Bluegamerdog - Backend API and bot host\nObviously_Shush - Frontend & Misc\nThe suffering - Bug testing")
+    embed.set_footer(text="DSB Helper 1.0")
+    embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/lnHKwZ40MRhYKuNJpNUS8NQiaekqkgfW9TaGj-B5Yg0/https/tr.rbxcdn.com/580b2e0cd698decfa585464b50a4278c/150/150/Image/Png")
+    await interaction.response.send_message(embed=embed, view=InfoboardView())
     
 @bot.tree.command(name="whois",description="Displays a user's information.")
 async def whois(interaction: discord.Interaction, user:discord.Member=None):
